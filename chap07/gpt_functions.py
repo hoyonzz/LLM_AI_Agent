56d0dfd79c054e76351b22bbf6fa9c0fc9c096aa
hoyong.shin <hoyong6737@gmail.com>
2025-12-30 19:23:12 +0900

    yfinance라이브러리 사용후 스트림릿 적용

--- chap07/gpt_functions.py
+++ chap07/gpt_functions.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pytz
+import yfinance as yf
 
 
 
@@ -10,6 +11,26 @@ def get_current_time(timezone: str = 'Asia/Seoul'):
     now_timezone = f'{now} {timezone}'
     print(now_timezone)
     return now_timezone
+
+def get_yf_stock_info(ticker: str):
+    stock = yf.Ticker(ticker)
+    info = stock.info
+    print(info)
+    return str(info)
+
+def get_yf_stock_history(ticker: str, period: str):
+    stock = yf.Ticker(ticker)
+    history = stock.history(period=period)
+    history_md = history.to_markdown()
+    print(history_md)
+    return history_md
+
+def get_yf_stock_recommendations(ticker: str):
+    stock = yf.Ticker(ticker)
+    recommendations = stock.recommendations
+    recommendations_md = recommendations.to_markdown()
+    print(recommendations_md)
+    return recommendations_md
 
 tools = [
     {
@@ -29,7 +50,66 @@ tools = [
             },
         }
     },
+    {
+        "type": "function",
+        "function": {
+            "name" : "get_yf_stock_info",
+            "description": "해당 종목의 Yahoo Finance 정보를 반환합니다.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    'ticker' : {
+                        'type': 'string',
+                        'description': 'Yahoo Finance 정보를 반환할 종목의 티커를 입력하세요. (예: AAPL)',
+                    },
+                },
+                "required": ['ticker']
+            }
+        }
+    },
+        {
+        "type": "function",
+        "function": {
+            "name" : "get_yf_stock_history",
+            "description": "해당 종목의 Yahoo Finance 주가 정보를 반환합니다.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    'ticker' : {
+                        'type': 'string',
+                        'description': 'Yahoo Finance 주가 정보를 반환할 종목의 티커를 입력하세요. (예: AAPL)',
+                    },
+                    'period' : {
+                        'type': 'string',
+                        'description': '주가 정보를 조회할 기간을 입력하세요. (예: 1d, 5d, 1mo, 1y, y5)',
+                    },
+                },
+                "required": ['ticker', 'period'],
+            },
+        }
+    },
+        {
+        "type": "function",
+        "function": {
+            "name" : "get_yf_stock_recommendations",
+            "description": "해당 종목의 Yahoo Finance 추천 정보를 반환합니다.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    'ticker' : {
+                        'type': 'string',
+                        'description': 'Yahoo Finance 추천 정보를 반환할 종목의 티커를 입력하세요. (예: AAPL)',
+                    },
+                },
+                "required": ['ticker']
+            }
+        }
+    },
 ]
 
 if __name__== '__main__':
-    get_current_time('America/New_York')
+    # get_current_time('America/New_York')
+    # info = get_yf_stock_info('AAPL')
+    get_yf_stock_history('AAPL', '5d')
+    print('----')
+    get_yf_stock_recommendations('AAPL')
