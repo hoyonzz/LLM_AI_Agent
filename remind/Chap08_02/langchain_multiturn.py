ef071e980abebc102474813a4e78af4494c6fd33
hoyong <hoyong0511@naver.com>
2026-05-13 15:47:09 +0900

    remind)langchain, RAG복습

---
+++ remind/Chap08_02/langchain_multiturn.py
@@ -0,0 +1,28 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+
+
+
+llm = ChatOpenAI(model='gpt-4o-mini')
+
+messages = [
+    SystemMessage("너는 사용자를 도와주는 상담사야.")
+]
+
+while True:
+    user_input = input("사용자: ")
+
+    if user_input == "exit":
+        break
+
+    messages.append(
+        HumanMessage(user_input)
+    )
+
+    ai_response = llm.invoke(messages)
+
+    messages.append(
+        ai_response
+    )
+
+    print("AI: " + ai_response.content)
